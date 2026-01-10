15caa6a9b5f85f29ea4d9ae0e90d4cfa789acf73
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2026-01-10 15:52:00 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld{
+
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello, my name is {FirstName} {LastName}.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "John";
+            person.LastName = "Doe";
+            person.Introduce();
+        }
+    }
+}
